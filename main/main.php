e17118a6c77e854c1998e4e8d9876b5cdaa693c5
resiliencelk <rajeev@resiliencelk.com>
2013-09-02 19:13:26 +0530

    hided the pwd

--- main/main.php
+++ main/main.php
@@ -9,7 +9,7 @@
 	
 	$dabasehandle=new _handledatabase();
 	$dabasehandle->_connectHost();
-	echo md5("832720860V");
+	//echo md5("832720860V");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
